2fb26ac7a7c4687608dd3715d551a6c264bb61fb
Daksh Yadav <ydaksh469@gmail.com>
2026-05-22 17:36:29 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let isLoggedIn="daksh"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"daksh"=>true
