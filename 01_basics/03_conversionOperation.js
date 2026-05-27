035981925543ef0bb04af06d3bcf297bb6679ff1
Daksh Yadav <ydaksh469@gmail.com>
2026-05-27 09:48:50 +0000

    Added JS basics and comparison files

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,6 +8,23 @@ console.log(valueInNumber);
 let isLoggedIn="daksh"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(3%2);
+let str1="hello"
+let str2=" world"
+console.log(str1+str2);
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+
 //1=>true;0=>false
 //""=>false
 //"daksh"=>true
