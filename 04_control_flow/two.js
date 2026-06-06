eef8c11d58d29d53bedb34cc4975933989f875db
Daksh Yadav <ydaksh469@gmail.com>
2026-06-06 06:53:19 +0000

    Switch

---
+++ 04_control_flow/two.js
@@ -0,0 +1,29 @@
+//Switch case statement
+
+// switch(key){
+//     case value1:
+//         //code to be executed if key === value1
+//         break;
+//     case value2:
+//         //code to be executed if key === value2
+//         break;
+//     default:
+//         //code to be executed if key doesn't match any case
+// }
+
+const month=3
+
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    default:
+        console.log("Invalid month");
+        break;
+}
