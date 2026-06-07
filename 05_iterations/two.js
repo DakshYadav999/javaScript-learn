62dbb8438df61460072260e0f457b341544961c6
Daksh Yadav <ydaksh469@gmail.com>
2026-06-07 08:54:03 +0000

    Do-While

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+//while
+
+// let index=0;
+// while(index<=10){
+//     console.log(`Value of index is ${index}`)
+//     index=index+2;
+// }
+
+// let i=0;
+// let myArray=["malenia","mohg","morgot"]
+// while(i<myArray.length){
+//     console.log(`Value is ${myArray[i]}`)
+//     i++;
+// }
+
+//Do
+
+let score=1;
+
+do{
+    console.log(score)
+    score++
+}while(score<10);
